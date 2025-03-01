6edcd812f60b210e3d001309b401e463f2c03b54
AlejandroHerr <ahernandezc@protonmail.com>
2025-03-02 00:23:44 +0100

    fix(fe): fix styling for now

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,12 +1,13 @@
 import path from 'path';
 
+import tailwindcss from '@tailwindcss/vite';
 import { TanStackRouterVite } from '@tanstack/router-plugin/vite';
 import viteReact from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [TanStackRouterVite(), viteReact()],
+  plugins: [TanStackRouterVite(), viteReact(), tailwindcss()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
